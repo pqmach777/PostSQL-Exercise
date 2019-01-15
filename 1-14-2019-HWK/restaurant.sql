30536046d1dd1031c3a6316a45026485e0ccb53b
Phong Mach <pqmach77@gmail.com>
2019-01-14 23:14:12 -0600

    created a databe using sql

---
+++ 1-14-2019-HWK/restaurant.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE restaurant (
+--     id SERIAL NOT NULL PRIMARY KEY,
+--     name varchar,
+--     distance decimal(4,2) and ("miles"),
+--     stars integer CHECK(stars < 6 AND stars > 0),
+--     category varchar,
+--     favorite_dish varchar,
+--     takeout char(1),
+--     last_visted_date date
+-- );
+-- UPDATE restaurant SET distance = CAST()
+-- INSERT INTO restaurant VALUES (
+--     DEFAULT, 'Moon Tower Inn', 0.2, 4, 'Game-meat & specialty hot dogs', 'Chong', 'Y', '12-18-2018'
+-- );
+-- INSERT INTO restaurant VALUES (
+--     DEFAULT, 'Hoodadak', 29.1, 5, 'Korean fried chicken & savory sides', 'Fried Chicken Wings Large(20)', 'Y', '1-12-2019'
+-- );
+-- INSERT INTO restaurant VALUES (
+--     DEFAULT, 'Hokkaido Japanese Kitchen', 14.5, 4, 'Sushi and Japanese dishes', 'Chopped Scallop', 'Y', '8-24-2018'
+-- );
+
